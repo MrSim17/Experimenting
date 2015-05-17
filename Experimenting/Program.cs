035b6abda242e9c5af3a2902e26ab3ae56d462fd
Nathan Corn <ncorn17@gmail.com>
2015-05-17 15:28:53 -0400

    Init Repo

---
+++ Experimenting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experimenting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void MultidimensionalArray()
+        {
+            // Two Dimensions
+            int[,] twoDim = new int[2,2];
+            twoDim[0, 0] = 0;
+            twoDim[0,1] = 1;
+            twoDim[1, 0] = 2;
+
+        }
+
+        public void JaggedArray()
+        {
+
+        }
+    }
+}
